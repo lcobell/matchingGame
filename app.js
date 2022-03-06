8fa150576b2497800d3edca80cdf3073e39359ee
lcobell <lcobell@gmail.com>
2022-03-06 13:52:34 -0600

    images resized
    resized images

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-// Get the players name 
-let userName = prompt('What is your name?') 
+document.addEventListener('DomContentLoaded', () => {
 
+<<<<<<< Updated upstream
 let cards = document.querySelectorAll('.card');
 
 for (var i =0; i < cards.length; i++) {
@@ -13,3 +13,85 @@ function flip(card) {
     console.log('flipping' + card.id);
     card.children[1].classList.toggle('flip');
 }
+=======
+
+
+
+// let card = document.getElementById('cardA')
+
+// document.getElementById('cardA').addEventListener('click', flip);
+
+// function flip() {
+//     document.getElementById('cardA').classList.toggle('flip');
+// }
+
+// card Array
+const ArrayOfCards = [
+    {
+        front: "fenceDog",
+        img: "images/dogWalkingThruFence.png", 
+    },
+    {
+        front: "fenceDog",
+        img: "images/dogWalkingThruFence.png",
+    },
+    {
+        front: "pointingDog",
+        img: "images/dogPointingAtCat.png"
+    },
+    {
+        front: "pointingDog",
+        img: "images/dogPointingAtCat.png"
+    },
+    {
+        front: "tvCat",
+        img: "images/catWatchingTv.png"
+    },
+    {
+        front: "tvCat",
+        img: "images/catWatchingTv.png"
+    },
+    {
+        front: "recycleDog",
+        img: "images/recycleDog.png"
+    },
+    {
+        front: "recycleDog",
+        img: "images/recycleDog.png"
+    },
+    {
+        front: "guardDog",
+        img: "images/guardDog.png"
+    },
+    {
+        front: "guardDog",
+        img: "images/guardDog.png"
+    },
+    {
+        front: "eduDog",
+        img: "images/onlineEdu.png"
+    },
+    {
+        front: "eduDog",
+        img: "images/onlineEdu.png"
+    }
+]
+
+
+const board = document.querySelector(".board")
+
+function setBoard () {
+    for (let i = 0; i < ArrayOfCards.length; i++) {
+        let card = document.createElement("img")
+        card.setAttribute("src", "images/cardBack.png")
+        card.setAttribute("data-id", i)
+        board.appendChild(card)
+        
+        //card.addEventListener("onclick", flip)
+
+    }
+}
+setBoard()
+
+})
+>>>>>>> Stashed changes
