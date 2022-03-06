2ba29ed42a435b4b47db69e15e46499be8693235
lcobell <lcobell@gmail.com>
2022-03-06 14:14:12 -0600

    all cards have event listeners
    all cards now flip...

--- app.js
+++ app.js
@@ -1,10 +1,48 @@
-// Get the players name 
-let userName = prompt('What is your name?') 
+let selectedCards = [];
 
-let card = document.getElementById('cardA')
+function flipCard(card) {
 
-document.getElementById('cardA').addEventListener('click', flip);
+    if (selectedCards.includes(card)) {
+        return;
+    }
 
-function flip() {
-    document.getElementById('cardA').classList.toggle('flip');
+    console.log('flipping ' + card.id);
+    card.children[1].classList.toggle('flip');
+
+    selectedCards.push(card);
+
+    if (selectedCards.length == 2) {
+       
+    }
+
 }
+
+function getCards() {
+    let cards = document.getElementsByClassName('card');
+    return cards;
+}
+
+function setupCards() {
+    let cards = getCards();
+    for (var i = 0; i < cards.length; i++) {
+        let card = cards[i];
+        console.log('adding listener for ' + card.id);
+        card.addEventListener('click', function () { flipCard(card); });
+    }
+}
+
+function main() {
+    // Get the players name 
+    let userName = 'abc'; //prompt('What is your name?') 
+
+    setupCards();
+
+    let cardsToMatch = getCards();
+
+    //while (cardsToMatch.length > 0) {
+
+    //}
+
+}
+
+main()
