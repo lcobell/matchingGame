e37ecc47db83e3407d808749e4dc38ff194aa32e
lcobell <lcobell@gmail.com>
2022-03-07 12:26:35 -0600

    The game works
    The game works, adding timer next

--- app.js
+++ app.js
@@ -1,48 +1,121 @@
 let selectedCards = [];
+let matches = [];
 
-function flipCard(card) {
+let cardInfos = [
+    {
+        'img': 'dogWalkingThroughFence.png',
+        'alt': 'Dog walking through a fence'
+    },
+    {
+        'img': 'dogPointingAtCat.png',
+        'alt': 'Dog pointing at cat'
+    },        
+    {
+        'img': 'dogWpups.png',
+        'alt': 'Dog with pups'
+    },
+    {
+        'img': 'recycleDog.png',
+        'alt': 'Dog recycling'
+    },
+    {
+        'img': 'guardDog.png',
+        'alt': 'Dog on guard'
+    },
+    {
+        'img': 'onlineEdu.png',
+        'alt': 'Dog learning online'
+    }
+]
+// Drawing the cards and appending them to the board
+function drawCard(cardInfo) {
+    let board = document.getElementById('board');
+ 
+    let div = document.createElement('div');
+    div.classList.value = 'card';
+    div.dataset.name = cardInfo.img;
+ 
+    let html = `<img src="images/${cardInfo.img}" alt="${cardInfo.alt}" width="100" height="100" class="cardFace"> \
+                 <img src="images/cardBack.png" alt="Blue and black geometric design" width="100" height="100" class="back">`;
+     div.innerHTML = html;
+     board.appendChild(div);
+  
+ }
 
+ // Interactive logic
+ function shuffleCards() {
+    for (var i = 0; i < cardInfos.length; i++) {
+        drawCard(cardInfos[i]);
+        drawCard(cardInfos[i]);
+        
+    }
+}
+
+
+ // toggle, html on top is turned off for the "flip"
+ function toggleCard(card) {
+    console.log('flipping ' + card.id);
+    card.children[1].classList.toggle('flip');
+}
+
+function checkWin() {
+    let cardsToMatch = getCards();
+    if (cardsToMatch.length == 0) {
+        alert('You win!');
+    }
+}
+function makeMove(card) {
+
+    // cannot select an already selected card
     if (selectedCards.includes(card)) {
         return;
     }
 
-    console.log('flipping ' + card.id);
-    card.children[1].classList.toggle('flip');
-
+    toggleCard(card);
     selectedCards.push(card);
 
-    //if (selectedCards.length == 2) {
-
-    //}
-
+     // if two cards are selected....
+     if (selectedCards.length == 2) {
+        let name1 = selectedCards[0].dataset.name;
+        let name2 = selectedCards[1].dataset.name;
+        if (name1 === name2) {
+            console.log('match found');
+            matches.push([...selectedCards]);
+            setTimeout(function() {
+                selectedCards[0].innerHTML = '';
+                selectedCards[0].classList.value = 'done';
+                selectedCards[1].innerHTML = '';
+                selectedCards[1].classList.value = 'done';
+                selectedCards = [];
+                checkWin();
+            }, 1000);
+        } else {
+            console.log('match miss');
+            setTimeout(function() {
+                toggleCard(selectedCards[0]);
+                toggleCard(selectedCards[1]);
+                selectedCards = [];
+            }, 1000);
+        }
+    }
 }
 
 function getCards() {
-    let cards = document.getElementsByClassName('card');
-    return cards;
+let cards = document.getElementsByClassName('card');
+return cards;
 }
-
 function setupCards() {
+    shuffleCards(); 
     let cards = getCards();
     for (var i = 0; i < cards.length; i++) {
         let card = cards[i];
         console.log('adding listener for ' + card.id);
-        card.addEventListener('click', function () { flipCard(card); });
+        card.addEventListener('click', function () { makeMove(card); });
     }
+    return cards;
 }
 
 function main() {
-    // Get the players name 
-    let userName = 'abc'; //prompt('What is your name?') 
-
-    setupCards();
-
-    let cardsToMatch = getCards();
-
-    //while (cardsToMatch.length > 0) {
-
-    //}
-
+    let unmatchedCards = setupCards();
 }
-
 main()
